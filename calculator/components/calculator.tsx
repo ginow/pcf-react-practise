a548f68cbb7a367916edd1f0b410a58b32c15c3a
Gino <gino.jameskutty@gmail.com>
2022-10-25 11:13:52 +0530

    calculator.ts

--- calculator/components/calculator.tsx
+++ calculator/components/calculator.tsx
@@ -1,16 +1,44 @@
-import * as React from "react";
 import { TextField } from "@fluentui/react/lib/TextField";
+import { Label } from "@fluentui/react/lib/Label";
+import * as React from "react";
 import { Stack } from "@fluentui/react/lib/Stack";
-export class CalculatorComponent extends React.Component {
+import { ServiceProvider } from "pcf-react";
+import { TaxCalculatorViewModel } from "../viewmodel/TaxCalculator";
+import { observer } from "mobx-react";
+import { IconButton } from "@fluentui/react/lib/Button";
+
+export interface CalculatorComponentProps {
+    serviceProvider: ServiceProvider;
+}
+export class CalculatorComponent extends React.Component<CalculatorComponentProps> {
     render(): JSX.Element {
+        const vm = this.props.serviceProvider.get("TaxCalculatorViewModel") as TaxCalculatorViewModel;
+        const { calculateMode, amount, tax, valueAfterTax } = vm;
         return (
-            <>
-                <Stack horizontal>
-                    <TextField label="Amount" type="number" />
-                    <TextField label="Tax %" type="number" />
-                    <TextField label="Amount after tax" type="number" />
-                </Stack>
-            </>
-        )
+            <Stack horizontal>
+                {calculateMode && (
+                    <>
+                        <TextField label="Amount" type="number" value={amount.toString()} onChange={vm.onChangeAmount} />
+                        <TextField label="Tax %" type="number" value={tax.toString()} onChange={vm.onChangeTax} />
+                        <TextField
+                            label="Amount After Tax"
+                            value={valueAfterTax.toString()}
+                            type="number"
+                            onChange={vm.onChangeValueAfterTax}
+                        />
+                        <IconButton iconProps={{ iconName: "Accept" }} onClick={vm.onAccept} />
+                        <IconButton iconProps={{ iconName: "Cancel" }} onClick={vm.onCancel} />
+                    </>
+                )}
+                {!calculateMode && (
+                    <>
+                        <Label>{valueAfterTax}</Label>
+                        <IconButton iconProps={{ iconName: "Calculator" }} onClick={vm.onCalculate} />
+                    </>
+                )}
+            </Stack>
+        );
     }
 }
+
+observer(CalculatorComponent);
